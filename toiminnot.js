ecb3cf9ad5ce78bad0ed8580a093caf15de65433
Arto Eskelinen <arto.eske@gmail.com>
2023-05-22 09:03:48 +0300

    toimintoa tekstikentälle

--- toiminnot.js
+++ toiminnot.js
@@ -1,1 +1,16 @@
-console.log("morjenttes hyypiö")
+let syote = document.getElementById("lukema").value;
+let muunnaPainike = document.getElementById("painike");
+let vastausRivi = document.getElementById("muunnos");
+
+muunnaPainike.addEventListener("click", tarkastus);
+
+function tarkastus(event){
+    event.preventDefault();
+    if(syote.length < 1){
+        vastausRivi.innerHTML = "Et antanut muunnettavaa arvoa.";
+        syote.value = "";
+    }
+    
+    vastausRivi.style.display = "block";
+    // vastausRivi.innerHTML = "painoit nappia";
+}
