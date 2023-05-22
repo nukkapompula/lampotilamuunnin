76c2ca0afe1526f4a2fdc4b3e875eda6fc7b323c
Arto Eskelinen <arto.eske@gmail.com>
2023-05-22 09:50:29 +0300

    arvon 0 syöttö mahdollista

--- toiminnot.js
+++ toiminnot.js
@@ -6,8 +6,9 @@ muunnaPainike.addEventListener("click", tarkastus);
 function tarkastus(event){
     event.preventDefault();
     let syote = document.getElementById("lukema").value;
+    let pituus = syote.length;
     syote = Number(syote);
-    if(isNaN(syote) == true || syote == 0){
+    if(isNaN(syote) == true || pituus < 1){
         vastausRivi.innerHTML = "Et antanut muunnokseen soveltuvaa arvoa.";
         document.getElementById("lukema").value = "";
     } else {
